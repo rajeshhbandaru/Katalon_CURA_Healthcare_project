c8266ae71feba87250e608ce91f1cabe6b81f78c
RajeshBandaru <93913628+rajeshhbandaru@users.noreply.github.com>
2024-01-24 08:54:55 +0530

    Add files via upload

---
+++ Test Suites/TListener_CHS_MakeAppointment/TListener_CHS_MakeAppointment.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TListener_CHS_MakeAppointment</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>53645fc0-9e6a-481a-8c0e-65456149e079</testSuiteGuid>
+   <testCaseLink>
+      <guid>a1a36f43-30de-4236-82df-5f031ebc213e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_CHS_MakeAppointment/TC_CHS_MakeAppointement_002</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/TS_CHS_Setup_TearDown/TS_Login_MakeAppointment_Setup_TearDown.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_Login_MakeAppointment_Setup_TearDown</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>005668ce-6546-49c4-89df-52bf5c04eaa7</testSuiteGuid>
+   <testCaseLink>
+      <guid>c341c448-3da6-4eec-9e97-5da9f971e978</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_CHS_Login/TC_CHS_Login_001</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>092ea4ee-633a-41f1-ae76-2e0dbb492109</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>1c191c30-a365-417c-b6f8-6ff2bee49e6a</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>914ed3d3-b1bd-4e1e-b858-f2a0c00a3dc7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_CHS_MakeAppointment/TC_CHS_MakeAppointement_002</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
